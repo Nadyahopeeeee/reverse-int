ce2115d56b4111deb1470a58f0f6f0d648720007
Nadyahopeeeee <red_mist_@mail.ru>
2022-08-04 12:14:58 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+
 module.exports = function reverse (n) {
-  
+  return Number(Math.abs(n).toString().split('').reverse().join(''));
 }
+
+ //n = Math.abs(n)
+ //n = String(n)
+ //n = n.split('').reverse().join('')
+ //n = Number(n);
+ //return n
